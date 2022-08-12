73cdd57bf929c16352c8c99b7fbde8ad3c4a499a
kirangautam45 <gautamkiran38@gmail.com>
2022-08-12 13:02:45 +0545

    Updates

---
+++ precache-manifest.4741edec41e61c742520a8e4abacb511.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "63556c54246510b394b9",
+    "url": "/To-Do-Application/static/css/main.65ddb8f6.chunk.css"
+  },
+  {
+    "revision": "63556c54246510b394b9",
+    "url": "/To-Do-Application/static/js/main.63556c54.chunk.js"
+  },
+  {
+    "revision": "9011f09f15d75131f742",
+    "url": "/To-Do-Application/static/js/1.9011f09f.chunk.js"
+  },
+  {
+    "revision": "acef184bf9fec8a7939f",
+    "url": "/To-Do-Application/static/js/runtime~main.acef184b.js"
+  },
+  {
+    "revision": "cd2e9cc851ab3c5779f2e6c746b412be",
+    "url": "/To-Do-Application/static/media/img1.cd2e9cc8.png"
+  },
+  {
+    "revision": "89d382902707314b9472dabd16b8da33",
+    "url": "/To-Do-Application/static/media/img2.89d38290.png"
+  },
+  {
+    "revision": "cd2e9cc851ab3c5779f2e6c746b412be",
+    "url": "/To-Do-Application/static/media/img3.cd2e9cc8.png"
+  },
+  {
+    "revision": "c64cca4086e10e13d281bb4994298711",
+    "url": "/To-Do-Application/static/media/img4.c64cca40.png"
+  },
+  {
+    "revision": "9c4ff0f076555c4dcf092c4252a6f23f",
+    "url": "/To-Do-Application/index.html"
+  }
+];
